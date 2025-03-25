85c85cea016fd98fc7134a15f5de239e2f18fac4
Ivan S Glazunov <ivansglazunov@deep.foundation>
2025-03-26 00:01:05 +0300

    initial track

--- index.js
+++ index.js
@@ -3,24 +3,20 @@
  */
 import { Association } from './association.js';
 import { all } from './is.js';
-import { types, type } from './relations.js';
-import { initTrack } from './track.js';
+import { type, types } from './relations.js';
 
-export * from './events.js';
-export * from './is.js';
 export * from './track.js';
+export * from './events.js';
 export * from './gets.js';
-export * from './sets.js';
+export * from './is.js';
 export * from './many.js';
 export * from './relations.js';
+export * from './sets.js';
 
 // Экспортируем deep как вызываемый экземпляр Association с методами из is.js и gets.js
 export const deep = new Association((...args) => new Association(...args));
 
-// Инициализируем Track с использованием deep
-initTrack(deep);
-
 // Экспортируем саму Association для обратной совместимости
-export { Association, all, types, type };
+export { all, Association, type, types };
 
 export default deep;
